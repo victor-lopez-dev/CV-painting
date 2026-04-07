32cb3590087032295bb38a09fe905cc76f49d3d3
Victor Lopez <contact@victorlopez.me>
2026-04-07 18:39:41 -0400

    added css file and javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+const header = document.querySelector('nav');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 80) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
