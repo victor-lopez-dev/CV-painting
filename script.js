eaf8cbaef6c4785bf8c386cc813ba5f3be477492
Victor Lopez <contact@victorlopez.me>
2026-04-18 23:59:28 -0400

    added commercial page

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ const header = document.querySelector('nav');
       header.classList.remove('scrolled');
     }
   });
+
+const exitBtn = document.querySelector('#exit-btn');
+const navModal = document.querySelector('#nav_modal');
+let hamburgerBtn = document.querySelector('#hamburger-btn');
+let modalLinks = document.querySelectorAll('.nav_modal--list');
+
+for (let i = 0; i < modalLinks.length; i++) {
+	modalLinks[i].addEventListener('click', function () {
+		navModal.classList.remove('show_modal');
+	});
+}
+
+hamburgerBtn.addEventListener('click', function () {
+	navModal.classList.add('show_modal');
+});
+
+exitBtn.addEventListener('click', function () {
+	navModal.classList.remove('show_modal');
+});
